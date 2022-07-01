954b9f47f5195c26ab76170d704e99a87e8e6387
Tianyi Yang <tianyiya@usc.edu>
2022-07-01 02:45:39 -0700

    Add files via upload

---
+++ CNN Model.py
@@ -0,0 +1,640 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+import os
+from glob import glob
+import itertools
+import fnmatch
+import random
+import matplotlib.pylab as plt
+import seaborn as sns
+import cv2
+import sklearn
+from sklearn import model_selection
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, StratifiedKFold, learning_curve, GridSearchCV
+from sklearn.metrics import confusion_matrix, make_scorer, accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+import keras
+from keras import backend as K
+from keras.callbacks import Callback, EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential, model_from_json
+from keras.optimizers import SGD, RMSprop, Adam, Adagrad, Adadelta
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization, Conv2D, MaxPool2D, MaxPooling2D
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[ ]:
+
+
+imagePatches = glob('C:/Users/RAZER/Downloads/IDC_regular_ps50_idx5/**/*.png', recursive=True)
+for filename in imagePatches[0:10]:
+    print(filename)
+
+
+# In[ ]:
+
+
+image_name = "C:/Users/RAZER/Downloads/IDC_regular_ps50_idx5/9135/1/9135_idx5_x1701_y1851_class1.png" #Image to be used as query
+def plotImage(image_location):
+    image = cv2.imread(image_name)
+    image = cv2.resize(image, (50,50))
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)); plt.axis('off')
+    return
+plotImage(image_name)
+
+
+# In[ ]:
+
+
+# Plot Multiple Images
+bunchOfImages = imagePatches
+i_ = 0
+plt.rcParams['figure.figsize'] = (10.0, 10.0)
+plt.subplots_adjust(wspace=0, hspace=0)
+for l in bunchOfImages[:25]:
+    im = cv2.imread(l)
+    im = cv2.resize(im, (50, 50)) 
+    plt.subplot(5, 5, i_+1) #.set_title(l)
+    plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB)); plt.axis('off')
+    i_ += 1
+
+
+# In[ ]:
+
+
+def randomImages(a):
+    r = random.sample(a, 4)
+    plt.figure(figsize=(16,16))
+    plt.subplot(131)
+    plt.imshow(cv2.imread(r[0]))
+    plt.subplot(132)
+    plt.imshow(cv2.imread(r[1]))
+    plt.subplot(133)
+    plt.imshow(cv2.imread(r[2])); 
+randomImages(imagePatches)
+
+
+# In[ ]:
+
+
+patternZero = '*class0.png'
+patternOne = '*class1.png'
+classZero = fnmatch.filter(imagePatches, patternZero)
+classOne = fnmatch.filter(imagePatches, patternOne)
+print("IDC(-)\n\n",classZero[0:5],'\n')
+print("IDC(+)\n\n",classOne[0:5])
+
+
+# In[ ]:
+
+
+def process_images(lowerIndex,upperIndex):
+    """
+    Returns two arrays: 
+        x is an array of resized images
+        y is an array of labels
+    """ 
+    x = []
+    y = []
+    WIDTH = 50
+    HEIGHT = 50
+    CHANNELS = 3
+    for img in imagePatches[lowerIndex:upperIndex]:
+        full_size_image = cv2.imread(img)
+        x.append(cv2.resize(full_size_image, (WIDTH,HEIGHT), interpolation=cv2.INTER_CUBIC))
+        if img in classZero:
+            y.append(0)
+        elif img in classOne:
+            y.append(1)
+        else:
+            return
+    return x,y
+
+
+# In[ ]:
+
+
+X, Y = process_images(0,10000)
+
+
+# In[ ]:
+
+
+df = pd.DataFrame()
+df["images"]=X
+df["labels"]=Y
+X2=df["images"]
+Y2=df["labels"]
+X2=np.array(X2)
+imgs0=[]
+imgs1=[]
+imgs0 = X2[Y2==0] # (0 = no IDC, 1 = IDC)
+imgs1 = X2[Y2==1] 
+
+
+# In[ ]:
+
+
+def describeData(a,b):
+    print('Total number of images: {}'.format(len(a)))
+    print('Number of IDC(-) Images: {}'.format(np.sum(b==0)))
+    print('Number of IDC(+) Images: {}'.format(np.sum(b==1)))
+    print('Percentage of positive images: {:.2f}%'.format(100*np.mean(b)))
+    print('Image shape (Width, Height, Channels): {}'.format(a[0].shape))
+describeData(X2,Y2)
+
+
+# In[ ]:
+
+
+dict_characters = {0: 'IDC(-)', 1: 'IDC(+)'}
+print(df.head(10))
+print("")
+print(dict_characters)
+
+
+# In[ ]:
+
+
+def plotOne(a,b):
+    """
+    Plot one numpy array
+    """
+    plt.subplot(1,2,1)
+    plt.title('IDC (-)')
+    plt.imshow(a[0])
+    plt.subplot(1,2,2)
+    plt.title('IDC (+)')
+    plt.imshow(b[0])
+plotOne(imgs0, imgs1) 
+
+
+# In[ ]:
+
+
+def plotTwo(a,b): 
+    """
+    Plot a bunch of numpy arrays sorted by label
+    """
+    for row in range(3):
+        plt.figure(figsize=(20, 10))
+        for col in range(3):
+            plt.subplot(1,8,col+1)
+            plt.title('IDC (-)')
+            plt.imshow(a[0+row+col])
+            plt.axis('off')       
+            plt.subplot(1,8,col+4)
+            plt.title('IDC (+)')
+            plt.imshow(b[0+row+col])
+            plt.axis('off')
+plotTwo(imgs0, imgs1) 
+
+
+# In[ ]:
+
+
+def plotHistogram(a):
+    """
+    Plot histogram of RGB Pixel Intensities
+    """
+    plt.figure(figsize=(10,5))
+    plt.subplot(1,2,1)
+    plt.imshow(a)
+    plt.axis('off')
+    plt.title('IDC(+)' if Y[1] else 'IDC(-)')
+    histo = plt.subplot(1,2,2)
+    histo.set_ylabel('Count')
+    histo.set_xlabel('Pixel Intensity')
+    n_bins = 30
+    plt.hist(a[:,:,0].flatten(), bins= n_bins, lw = 0, color='r', alpha=0.5);
+    plt.hist(a[:,:,1].flatten(), bins= n_bins, lw = 0, color='g', alpha=0.5);
+    plt.hist(a[:,:,2].flatten(), bins= n_bins, lw = 0, color='b', alpha=0.5);
+plotHistogram(X2[100])
+
+
+# In[ ]:
+
+
+X=np.array(X)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=11)
+
+# Reduce Sample Size for DeBugging
+X_train = X_train[0:300000] 
+Y_train = Y_train[0:300000]
+X_test = X_test[0:300000] 
+Y_test = Y_test[0:300000]
+
+print("Training Data Shape:", X_train.shape)
+print("Testing Data Shape:", X_test.shape)
+
+
+# In[ ]:
+
+
+plotHistogram(X_train[100])
+
+
+# In[ ]:
+
+
+# Encode labels to hot vectors (ex : 2 -> [0,0,1,0,0,0,0,0,0,0])
+Y_trainHot = to_categorical(Y_train, num_classes = 2)
+Y_testHot = to_categorical(Y_test, num_classes = 2)
+
+
+# In[ ]:
+
+
+lab = df['labels']
+dist = lab.value_counts()
+sns.countplot(lab)
+print(dict_characters)
+
+
+# In[ ]:
+
+
+# Deal with imbalanced class sizes below
+# Make Data 1D for compatability upsampling methods
+X_trainShape = X_train.shape[1]*X_train.shape[2]*X_train.shape[3]
+X_testShape = X_test.shape[1]*X_test.shape[2]*X_test.shape[3]
+X_trainFlat = X_train.reshape(X_train.shape[0], X_trainShape)
+X_testFlat = X_test.reshape(X_test.shape[0], X_testShape)
+
+
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
+#ros = RandomOverSampler(ratio='auto')
+ros = RandomUnderSampler(sampling_strategy='auto')
+X_trainRos, Y_trainRos = ros.fit_resample(X_trainFlat, Y_train)
+X_testRos, Y_testRos = ros.fit_resample(X_testFlat, Y_test)
+
+# Encode labels to hot vectors (ex : 2 -> [0,0,1,0,0,0,0,0,0,0])
+Y_trainRosHot = to_categorical(Y_trainRos, num_classes = 2)
+Y_testRosHot = to_categorical(Y_testRos, num_classes = 2)
+
+
+for i in range(len(X_trainRos)):
+    height, width, channels = 50,50,3
+    X_trainRosReshaped = X_trainRos.reshape(len(X_trainRos),height,width,channels)
+
+for i in range(len(X_testRos)):
+    height, width, channels = 50,50,3
+    X_testRosReshaped = X_testRos.reshape(len(X_testRos),height,width,channels)
+
+dfRos = pd.DataFrame()
+dfRos["labels"]=Y_trainRos
+labRos = dfRos['labels']
+distRos = lab.value_counts()
+sns.countplot(labRos)
+print(dict_characters)
+
+
+# In[ ]:
+
+
+from sklearn.utils import class_weight
+class_weight1 = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(Y_train), y=Y_train)
+print("Old Class Weights: ",class_weight1)
+class_weight1_d = {0:class_weight1[0],1:class_weight1[1]}
+from sklearn.utils import class_weight
+class_weight2 = class_weight.compute_class_weight(class_weight='balanced',classes= np.unique(Y_trainRos),y= Y_trainRos)
+print("New Class Weights: ",class_weight2)
+class_weight2_d = {0:class_weight2[0],1:class_weight2[1]}
+
+
+# In[ ]:
+
+
+#Helper Functions  Learning Curves and Confusion Matrix
+
+class MetricsCheckpoint(Callback):
+    """Callback that saves metrics after each epoch"""
+    def __init__(self, savepath):
+        super(MetricsCheckpoint, self).__init__()
+        self.savepath = savepath
+        self.history = {}
+    def on_epoch_end(self, epoch, logs=None):
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
+        np.save(self.savepath, self.history)
+
+def plotKerasLearningCurve():
+    plt.figure(figsize=(10,5))
+    metrics = np.load('logs.npy',allow_pickle=True)[()]
+    filt = ['acc'] # try to add 'loss' to see the loss learning curve
+    for k in filter(lambda x : np.any([kk in x for kk in filt]), metrics.keys()):
+        l = np.array(metrics[k])
+        plt.plot(l, c= 'r' if 'val' not in k else 'b', label='val' if 'val' in k else 'train')
+        x = np.argmin(l) if 'loss' in k else np.argmax(l)
+        y = l[x]
+        plt.scatter(x,y, lw=0, alpha=0.25, s=100, c='r' if 'val' not in k else 'b')
+        plt.text(x, y, '{} = {:.4f}'.format(x,y), size='15', color= 'r' if 'val' not in k else 'b')   
+    plt.legend(loc=4)
+    plt.axis([0, None, None, None]);
+    plt.grid()
+    plt.xlabel('Number of epochs')
+    
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure(figsize = (5,5))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+def plot_learning_curve(history):
+    plt.figure(figsize=(8,8))
+    plt.subplot(1,2,1)
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig('./accuracy_curve.png')
+    #plt.clf()
+    # summarize history for loss
+    plt.subplot(1,2,2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig('./loss_curve.png')
+
+
+# In[ ]:
+
+
+def runKerasCNN(a,b,c,d,e,f):
+    """
+    Run Keras CNN: https://github.com/fchollet/keras/blob/master/examples/mnist_cnn.py
+    """
+    batch_size = 256
+    num_classes = 2
+    epochs = 12
+#     img_rows, img_cols = a.shape[1],a.shape[2]
+    img_rows,img_cols=50,50
+    input_shape = (img_rows, img_cols, 3)
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape,strides=e))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['acc'])
+    imagegen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=True)  # randomly flip images
+    history=model.fit(imagegen.flow(a,b, batch_size=16),
+                        steps_per_epoch=len(a) / 16, epochs=epochs,class_weight=f, validation_data = [c, d],callbacks = [MetricsCheckpoint('logs')])
+    score = model.evaluate(c,d, verbose=0)
+    print('\nKeras CNN #1C - accuracy:', score[1],'\n')
+    y_pred = model.predict(c)
+    map_characters = {0: 'IDC(-)', 1: 'IDC(+)'}
+    print('\n', sklearn.metrics.classification_report(np.where(d > 0)[1], np.argmax(y_pred, axis=1), target_names=list(map_characters.values())), sep='')    
+    Y_pred_classes = np.argmax(y_pred,axis=1) 
+    Y_true = np.argmax(d,axis=1) 
+    plotKerasLearningCurve()
+    plt.show()  
+    plot_learning_curve(history)
+    plt.show()
+    confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
+    plot_confusion_matrix(confusion_mtx, classes = list(dict_characters.values())) 
+    plt.show()
+    return model
+
+
+# In[ ]:
+
+
+def BuildCNN_2(num_classes):
+    
+    num_classes = 2
+#     img_rows, img_cols = a.shape[1],a.shape[2]
+    img_rows,img_cols=50,50
+    input_shape = (img_rows, img_cols, 3)
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape,strides=2))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss=keras.losses.binary_crossentropy,
+                  optimizer=Adam(learning_rate=1e-4),
+                  metrics=['acc'])
+    
+    
+    return model
+
+    
+
+
+# In[ ]:
+
+
+from keras import layers
+from keras.applications import ResNet50,MobileNet, DenseNet201, InceptionV3, NASNetLarge, InceptionResNetV2, NASNetMobile
+
+
+# In[ ]:
+
+
+def BuildCNN_3():
+    model = Sequential()
+    ResNet = DenseNet201(
+    weights='imagenet',
+    include_top=False,
+    input_shape=(50,50,3)
+    )
+    model.add(ResNet)
+    model.add(layers.GlobalAveragePooling2D())
+    model.add(layers.Dropout(0.5))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(2, activation='softmax'))
+    
+    
+    model.compile(loss=keras.losses.binary_crossentropy,
+                  optimizer=Adam(learning_rate=1e-4),
+                  metrics=['acc'])
+    
+    return model
+
+
+# In[ ]:
+
+
+def BuildCNN_4():
+    model = Sequential()
+    ResNet = DenseNet201(
+    weights='imagenet',
+    include_top=False,
+    input_shape=(50,50,3)
+    )
+    model.add(ResNet)
+    model.add(layers.GlobalAveragePooling2D())
+    model.add(layers.Dropout(0.25))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(2, activation='softmax'))
+    
+    
+    model.compile(loss=keras.losses.binary_crossentropy,
+                  optimizer=Adam(learning_rate=1e-4),
+                  metrics=['acc'])
+    
+    return model
+
+
+# In[ ]:
+
+
+model = BuildCNN_3()
+model.summary()
+
+
+# In[ ]:
+
+
+def runModel(model,a,b,c,d,e):
+    
+    epochs = 22
+    
+    learn_control = ReduceLROnPlateau(monitor='val_acc', patience=5,
+                                  verbose=1,factor=0.2, min_lr=1e-7)
+    early_stopping_monitor = EarlyStopping(monitor='val_acc', patience=8, mode='max')
+    model_checkpoint = ModelCheckpoint('cnn_7.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+    
+    imagegen = ImageDataGenerator(
+        rotation_range=90,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=True)  # randomly flip images
+    history=model.fit(imagegen.flow(a,b, batch_size=16),
+                        steps_per_epoch=len(a) / 16, epochs=epochs,class_weight=e, validation_data = [c, d],callbacks = [learn_control,early_stopping_monitor,model_checkpoint])
+    score = model.evaluate(c,d, verbose=0)
+    print('\nKeras CNN - accuracy:', score[1],'\n')
+    y_pred = model.predict(c)
+    map_characters = {0: 'IDC(-)', 1: 'IDC(+)'}
+    print('\n', sklearn.metrics.classification_report(np.where(d > 0)[1], np.argmax(y_pred, axis=1), target_names=list(map_characters.values())), sep='')    
+    Y_pred_classes = np.argmax(y_pred,axis=1) 
+    Y_true = np.argmax(d,axis=1) 
+    plot_learning_curve(history)
+    plt.show()
+    confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
+    plot_confusion_matrix(confusion_mtx, classes = list(dict_characters.values())) 
+    plt.show()
+
+
+# In[ ]:
+
+
+runModel(model,X_train, Y_trainHot, X_test, Y_testHot,class_weight1_d)
+#runModel(model,X_trainRosReshaped, Y_trainRosHot, X_testRosReshaped, Y_testRosHot,class_weight2_d)
+
+
+# In[ ]:
+
+
+model = BuildCNN_2(num_classes=2)
+
+
+# In[ ]:
+
+
+model.summary()
+
+
+# In[ ]:
+
+
+model.save("cnn_2.h5")
+
+
+# In[ ]:
+
+
+from PIL import Image
+import tensorflow as tf
+
+#img = Image.open("C:/Users/RAZER/Downloads/IDC_regular_ps50_idx5/9135/1/9135_idx5_x1701_y1851_class1.png")
+img = X_test[98]
+img = tf.keras.preprocessing.image.img_to_array(img)
+img = tf.image.resize(img,[50,50])
+img = tf.expand_dims(img,axis=0)
+y_pred = model.predict(img)
+print(y_pred)
+
+
+# In[ ]:
+
+
+
+
+
+# 
